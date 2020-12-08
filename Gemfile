9caa4e9199cecd753a2c2c3aaf00a4be6b7f52c5
denisshirokikh <denisshirokikh@gmail.com>
2020-12-07 17:22:19 -0800

    48-2 comments resolved

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 gem 'jquery-rails'
+gem 'webpacker', '~> 5.x'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
   VALID_EMAIL = /\A\w+@\w+\.\w+\z/i
-  VALID_USERNAME = /\A[a-z0-9_]\z/i
+  VALID_USERNAME = /\A[a-z_]\z/i
+
   attr_accessor :password
 
   before_validation :string_downcase
@@ -20,6 +21,22 @@ class User < ApplicationRecord
             length: {maximum: 40}
   validates :password, presence: true, confirmation: true, on: :create
 
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    return nil unless user.present?
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+    return user if user.password_hash == hashed_password
+    nil
+  end
+
   private
 
   def encrypt_password
@@ -33,20 +50,7 @@ class User < ApplicationRecord
 
     end
   end
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
-  def self.authenticate(email, password)
-    user = find_by(email: email)
-    return nil unless user.present?
-    hashed_password = User.hash_to_string(
-      OpenSSL::PKCS5.pbkdf2_hmac(
-        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
-      )
-    )
-    return user if user.password_hash == hashed_password
-    nil
-  end
+
   def string_downcase
     email&.downcase!
     username&.downcase!
