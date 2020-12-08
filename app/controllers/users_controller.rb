19976c9dbf73daae94fcd7d75f9f0d3b190b3fdd
denisshirokikh <denisshirokikh@gmail.com>
2020-12-07 16:05:39 -0800

    lesson 49

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
   def new
@@ -12,7 +22,16 @@ class UsersController < ApplicationController
     @user = User.new(
       name: 'Vadim',
       username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+      avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
     )
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
+      )
+    ]
+
+    @new_question = Question.new
   end
 end
