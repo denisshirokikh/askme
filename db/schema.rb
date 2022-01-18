5d97083c18683cded931f7c0d7b933dda6858cda
Denis Shirokikh <denis.shirokikh@omf.com>
2022-01-18 12:10:08 -0600

    gem update, removed chosing of colol on registration page

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_12_27_033747) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.integer "author_id"
-    t.index ["author_id"], name: "index_questions_on_author_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
